807eb3e8f4ca0472f1beb00186742112474a8ebe
voshchinskiyvitya <voshchinskiyvitya@ukr.net>
2016-08-04 21:49:10 +0300

    Remove batch files.

--- batch-files/moveApp.bat
+++
@@ -1,3 +0,0 @@
-move /y "f:\Projects\HungryStudent\compiled\App.js" "f:\Projects\HungryStudent\site\"
-
-xcopy f:\Projects\HungryStudent\src\*.* f:\Projects\HungryStudent\site\ /y /exclude:f:\Projects\HungryStudent\batch-files\excluded.txt
