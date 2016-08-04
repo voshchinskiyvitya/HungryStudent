2a8fc63e36d4204ef9a372c1dee33bfa8ef718b9
voshchinskiyvitya <voshchinskiyvitya@ukr.net>
2016-08-04 15:38:16 +0300

    Delete batch files (old copy)

--- batch files/moveApp.bat
+++
@@ -1,5 +0,0 @@
-move /y "f:\Projects\HungryStudent\compiled\App.js" "f:\Projects\HungryStudent\site\"
-
-copy /y "f:\Projects\HungryStudent\src\styles.css" "f:\Projects\HungryStudent\site\"
-
-copy /y "f:\Projects\HungryStudent\src\index.html" "f:\Projects\HungryStudent\site\"
