4c2122fe15f4e4587f538ef958eb5f0c3c0a0a36
voshchinskiyvitya <voshchinskiyvitya@ukr.net>
2016-08-04 23:23:21 +0300

    Add logo
    - add logo
    - adjust sql scripts

--- Scripts/TableDefinition.sql
+++ Scripts/TableDefinition.sql
@@ -1,23 +1,23 @@
-CREATE TABLE [Products](
-	[ID] INT NOT NULL AUTO_INCREMENT,
-	[Name] VARCHAR(255),
+CREATE TABLE `test_db`.`Products`(
+	`ID` INT NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(255),
 	PRIMARY KEY (ID)
-)
+);
 
-CREATE TABLE [Recipes](
-	[ID] INT NOT NULL AUTO_INCREMENT,
-	[Name] VARCHAR(255),
-	[Description] VARCHAR(5000),
+CREATE TABLE `test_db`.`Recipes`(
+	`ID` INT NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(255),
+	`Description` VARCHAR(5000),
 	PRIMARY KEY (ID)
-)
+);
 
-CREATE TABLE [RecipeProducts](
-	[ID] INT NOT NULL AUTO_INCREMENT,
-	[ProductID] INT NOT NULL,
-	[RecipeID] INT NOT NULL,
-	[Quantity] INT,
-	[QuantitySuffix] VARCHAR(255),
+CREATE TABLE `test_db`.`RecipeProducts`(
+	`ID` INT NOT NULL AUTO_INCREMENT,
+	`ProductID` INT NOT NULL,
+	`RecipeID` INT NOT NULL,
+	`Quantity` INT,
+	`QuantitySuffix` VARCHAR(255),
 	PRIMARY KEY (ID),
-	FOREIGN KEY [ProductID] REFERENCES [Products]([ID]),
-	FOREIGN KEY [RecipeID] REFERENCES [Recipes]([ID])
-)
+	FOREIGN KEY (`ProductID`) REFERENCES `test_db`.`Products`(`ID`),
+	FOREIGN KEY (`RecipeID`) REFERENCES `test_db`.`Recipes`(`ID`)
+);
