28494b004bd6273d96a1f00c9c7ced89b1ed2d55
voshchinskiyvitya <voshchinskiyvitya@ukr.net>
2016-08-04 21:45:08 +0300

    Revert "Remove batch files."

--- batch-files/moveApp.bat
+++ batch-files/moveApp.bat
@@ -1,3 +1,3 @@
-move /y "f:\Projects\HungryStudent\compiled\App.js" "f:\Projects\HungryStudent\site\"
+move /y "c:\Users\User\Desktop\Projects\HungryStudent\compiled\App.js" "c:\Users\User\Desktop\Projects\HungryStudent\site\"
 
-xcopy f:\Projects\HungryStudent\src\*.* f:\Projects\HungryStudent\site\ /y /exclude:f:\Projects\HungryStudent\batch-files\excluded.txt
+xcopy c:\Users\User\Desktop\Projects\HungryStudent\src\*.* c:\Users\User\Desktop\Projects\HungryStudent\site\ /y /exclude:c:\Users\User\Desktop\Projects\HungryStudent\batch-files\excluded.txt
