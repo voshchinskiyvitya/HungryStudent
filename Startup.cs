3e113a777b4d05100fb0f0e12d9ee8e16ae41c6d
voshchinskiyvitya <voshchinskiyvitya@ukr.net>
2016-09-27 17:54:06 +0300

    ASP MVC

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HungryStudent.Startup))]
+namespace HungryStudent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
